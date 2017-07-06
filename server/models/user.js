d02473757fb070893ff65bd6a5c67e933eb7add0
L <L>
2017-07-06 17:59:03 +0100

    add test cases

--- server/models/user.js
+++ server/models/user.js
@@ -1,1 +1,14 @@
-ECHO is on.
+const mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+  email:{
+    type:String,
+    required:true,
+    minlength:1,
+    trim:true
+  }
+});
+
+var User = mongoose.model('User',UserSchema);
+
+module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -1,12 +1,48 @@
-const mongoose = require('mongoose');
 const express = require('express');
 var app = express();
+const bodyParser = require('body-parser');
+
+const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
+const {User} = require('./models/User');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/todoApp',{useMongoClient:true});
 
-Todo.create({
-  text:'Hey now you are an all star',
-  completed:false
-})
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+  Todo.create(req.body).then((todo)=>{
+    res.send(todo);
+  },(err)=>{
+    res.sendStatus(400);
+  }).catch((err)=>{
+    res.sendStatus(400);
+  })
+});
+
+app.get('/todos',(req,res)=>{
+  Todo.find({}).then((todos)=>{
+    res.send(todos)
+  },(err)=>{
+    res.sendStatus(400);
+  }).catch((err)=>{
+    res.sendStatus(400);
+  })
+});
+
+app.get('/todos/:id',(req,res)=>{
+  var id = req.body.id;
+  Todo.findOne(id).then((todo)=>{
+    res.send(todo);
+  },(err)=>{
+    res.sendStatus(400);
+  }).catch((err)=>{
+    res.sendStatus(400);
+  })
+});
+
+
+app.listen(process.env.PORT || 3000,()=>{
+  console.log('listening on port 3000');
+});
+
+module.exports = {app}
