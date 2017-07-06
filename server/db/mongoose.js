e3d39f80e306c807fa9166189bf1cacd102b6f77
L <L>
2017-07-06 19:33:22 +0100

    add db link

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-
+var dbLink = process.env.MONGODB || 'mongodb://localhost/todoApp';
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/todoApp',{useMongoClient:true});
+mongoose.connect(dbLink,{useMongoClient:true});
 
 module.exports = {mongoose};
